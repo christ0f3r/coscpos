2d6c38893f0c8beacb14a5ecf413147077d6ecaf
Joshua Batac <joshuabatac@Joshuas-MacBook-Pro-40.local>
2023-10-26 17:35:11 -0500

    css to manger view

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/server_view', (req, res) => {
+  res.render('server_view');
+});
+
 app.get('/manager_view', (req, res) => {
   res.render('manager_view');
 });
 
-app.get('/server_view', (req, res) => {
-  res.render('server_view');
-});
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
