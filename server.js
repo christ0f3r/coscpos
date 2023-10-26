e52b54ae5c2266fbdcb095fbd69b69822de0fdba
Joshua Batac <joshuabatac@Joshuas-MBP-40.mynetworksettings.com>
2023-10-25 19:19:40 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/manager_view', (req, res) => {
+  res.render('manager_view');
+});
+
+app.get('/server_view', (req, res) => {
+  res.render('server_view');
+});
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
