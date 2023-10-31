877550d0689316673ee8f659331c5ec67a388ad9
zainbaig0529 <122945572+zainbaig0529@users.noreply.github.com>
2023-10-30 22:48:10 -0500

    changes at 1048

--- server.js
+++ server.js
@@ -61,33 +61,6 @@ app.get('/inventory_Report', (req, res) => {
   });
 });
 
-
-// In your Express route handler
-app.get('/sales-report', function(req, res) {
-  // Sample sales data with date information
-  var salesData = [
-      { date: '2023-10-01', grossSales: 1000, discounts: 100, netSales: 900, tax: 90, creditCard: 500, debit: 300, cash: 100 },
-      { date: '2023-10-02', grossSales: 1200, discounts: 150, netSales: 1050, tax: 105, creditCard: 600, debit: 350, cash: 100 },
-      { date: '2023-10-03', grossSales: 800, discounts: 80, netSales: 720, tax: 72, creditCard: 400, debit: 200, cash: 120 },
-      // Add more sales data as needed
-  ];
-
-  res.render('sales_view', { salesData: salesData });
-});
-
-// Handle the form submission to filter data based on the selected time period
-app.post('/generate-report', function(req, res) {
-  const startDate = new Date(req.body.startDate);
-  const endDate = new Date(req.body.endDate);
-
-  // Filter the sample sales data based on the selected time period
-  const filteredSalesData = salesData.filter(item => {
-      const itemDate = new Date(item.date);
-      return itemDate >= startDate && itemDate <= endDate;
-  });
-
-  res.render('sales_view', { salesData: filteredSalesData });
-});
  
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
